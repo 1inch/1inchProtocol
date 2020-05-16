9b2af558361347a35353b543008ad83e577339ea
realisation <realisation@realisations-MacBook-Pro-2.local>
2020-05-15 17:24:49 -0700

    Removed trailing whitespace in IShell

--- contracts/interface/IShell.sol
+++ contracts/interface/IShell.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.5.0;
 interface IShell {
 
     function viewOriginTrade(
-        address origin, 
-        address target, 
+        address origin,
+        address target,
         uint256 originAmount
     ) external view returns (uint256);
 
     function swapByOrigin(
-        address origin, 
-        address target, 
-        uint256 originAmount, 
-        uint256 minTargetAmount, 
+        address origin,
+        address target,
+        uint256 originAmount,
+        uint256 minTargetAmount,
         uint256 deadline
     ) external returns (uint256);
 
