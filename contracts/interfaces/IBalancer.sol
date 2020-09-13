665b68a539ecc935c709b019df522b57abe5b5dc
Anton Bukov <k06aaa@gmail.com>
2020-09-13 14:54:26 +0300

    New Balancer Registry

--- contracts/interfaces/IBalancer.sol
+++ contracts/interfaces/IBalancer.sol
@@ -22,49 +22,6 @@ interface IBalancerPool {
 }
 
 interface IBalancerRegistry {
-    // Get info about pool pair for 1 SLOAD
-    function getPairInfo(address pool, IERC20 fromToken, IERC20 destToken)
-        external view returns(uint256 weight1, uint256 weight2, uint256 swapFee);
-
-    // Pools
-    function checkAddedPools(address pool)
-        external view returns(bool);
-    function getAddedPoolsLength()
-        external view returns(uint256);
-    function getAddedPools()
-        external view returns(address[] memory);
-    function getAddedPoolsWithLimit(uint256 offset, uint256 limit)
-        external view returns(address[] memory result);
-
-    // Tokens
-    function getAllTokensLength()
-        external view returns(uint256);
-    function getAllTokens()
-        external view returns(address[] memory);
-    function getAllTokensWithLimit(uint256 offset, uint256 limit)
-        external view returns(address[] memory result);
-
-    // Pairs
-    function getPoolsLength(IERC20 fromToken, IERC20 destToken)
-        external view returns(uint256);
-    function getPools(IERC20 fromToken, IERC20 destToken)
-        external view returns(IBalancerPool[] memory);
-    function getPoolsWithLimit(IERC20 fromToken, IERC20 destToken, uint256 offset, uint256 limit)
-        external view returns(IBalancerPool[] memory result);
-    function getBestPools(IERC20 fromToken, IERC20 destToken)
-        external view returns(IBalancerPool[] memory pools);
     function getBestPoolsWithLimit(IERC20 fromToken, IERC20 destToken, uint256 limit)
         external view returns(IBalancerPool[] memory pools);
-
-    // Get swap rates
-    function getPoolReturn(address pool, IERC20 fromToken, IERC20 destToken, uint256 amount)
-        external view returns(uint256);
-    function getPoolReturns(address pool, IERC20 fromToken, IERC20 destToken, uint256[] calldata amounts)
-        external view returns(uint256[] memory result);
-
-    // Add and update registry
-    function addPool(address pool) external returns(uint256 listed);
-    function addPools(address[] calldata pools) external returns(uint256[] memory listed);
-    function updatedIndices(address[] calldata tokens, uint256 lengthLimit) external;
 }
-
